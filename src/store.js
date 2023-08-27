e4d0e8dfe2eedfa7413823142a174222667b9920
Mohammad <mfalahat2002@gmail.com>
2023-08-27 21:14:24 +0330

    add vuex file to add state

---
+++ src/store.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+
+        usersData: JSON.parse(localStorage.getItem('usersData')) || [],
+    },
+    mutations: {
+        addUser(state, newUser) {
+            state.usersData.push(newUser);
+            localStorage.setItem('usersData', JSON.stringify(state.usersData));
+        },
+        removeUser(state, index) {
+            state.usersData.splice(index, 1);
+            localStorage.setItem('usersData', JSON.stringify(state.usersData));
+        },
+        editUser(state, index) {
+            state.usersData[index].editing = true;
+            localStorage.setItem('usersData', JSON.stringify(state.usersData));
+        },
+        saveUser(state, index) {
+            state.usersData[index].editing = false;
+            localStorage.setItem('usersData', JSON.stringify(state.usersData));
+        },
+        setUsersData(state, data) {
+            state.usersData = data;
+            localStorage.setItem('usersData', JSON.stringify(state.usersData));
+        }
+    },
+    actions: {
+    getUsersData({ commit }) {
+        const data = JSON.parse(localStorage.getItem('usersData')) || [];
+        commit('setUsersData', data);
+    }
+    },
+    getters: {},
+})
