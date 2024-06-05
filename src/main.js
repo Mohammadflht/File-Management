35a131bd7dc9d51403f30b77c843821b37f74e1d
mohammad <mfalahat2002@gmail.com>
2024-06-05 18:19:04 +0330

    ...

--- src/main.js
+++ src/main.js
@@ -5,33 +5,21 @@ import router from './router'
 // import vuetify from "./vuetify";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
 
-/* import font awesome icon component */
+import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+library.add(fas, fab)
 
 // Call this action to get the data from localStorage on page load
 store.dispatch('getUsersData');
 
-/* add icons to the library */
-library.add(faUserSecret, faTwitter, fas, fab)
-
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-
-
-
-
-
-
 
 
 Vue.config.productionTip = false
