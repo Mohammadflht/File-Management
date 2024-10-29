2071cf2262f5180a25e93f9b128f53b588109823
mohammad <mfalahat2002@gmail.com>
2024-10-29 19:30:31 +0330

    ....

--- src/router.js
+++ src/router.js
@@ -10,15 +10,17 @@ import UserPanel from './components/UserPanel.vue'
 Vue.use(VueRouter);
 
 const routes =[
-    {path: '/', component: LoginPage},
+    {path: '/login', name: 'login', component: LoginPage},
     {path: '/admin-panel/user-management', component: UserManagement},
     {path: '/admin-panel/user-group', component: UserGroup},
     {path: '/admin-panel/storage-space', component: StorageSpace},
     {path: '/admin-panel/file-management', component: FileManagement},
     {path: '/user-panel/file-management', component: UserPanel},
+    {path:'*', redirect: {name: 'login'}},
 ];
 const router = new VueRouter({
-    routes
+    routes,
+    mode: 'history',
 });
 
 export default router;
